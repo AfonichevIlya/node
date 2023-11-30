ef98633d0be96e556be20f40b3d9f647275f347a
= <=>
2023-11-30 15:24:23 +0300

    commit

--- controllers/register.js
+++ controllers/register.js
@@ -1,3 +1,7 @@
+const User = require("../models/user");
 exports.form = (req, res) => {
   res.render(`register`, {});
+};
+exports.submit = (req, res, next) => {
+  User.create(req.body.user, cb);
 };
