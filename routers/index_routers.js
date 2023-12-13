05ad5348fbc9ec75fc2b1e9570b06cf0d8f415aa
= <=>
2023-12-13 15:30:01 +0300

    commit

--- routers/index_routers.js
+++ routers/index_routers.js
@@ -4,14 +4,14 @@ const register = require("../controllers/register");
 const login = require("../controllers/login");
 const logout = require("../controllers/logout");
 const entries = require("../controllers/entries");
-const entry = require("../controllers/entry");
+const entry = require("../models/entry");
 const post = require("../controllers/post");
 const index = require("../controllers/index");
 
 router.get("/", entries.list);
 
-// router.post("/entry", entries.form);
-// router.post("/post", entries.post);
+router.post("/entry", entries.form);
+router.post("/post", entries.submit);
 
 router.get("/register", register.form);
 router.post("/register", register.submit);
